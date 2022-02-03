2639ca6f65f4bc328c087ea92dc0dd82b2c52510
Issei <ttsw099@gmail.com>
2022-02-03 16:07:54 +0900

    edit link

--- site.config.js
+++ site.config.js
@@ -47,5 +47,22 @@ module.exports = {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null
+  pageUrlOverrides: {
+    'technology': 'bf7604b81aeb47ccbafb0680e0b87d9d',
+    'coding': 'c8aab68522e64c33890994a91cd4c621',
+    'pm': '8a05ffbc78f941e7b0cd285ba3504647',
+    'design&art': '978d03c5ca0e49f9937e121cb0001624',
+    'business': '9bdc9e104e0f4b2e8f6bf6a4bad54e54',
+    'blog': '21a94fa3aa6f4d0eb067629e694844a8',
+    'Arts': '20163dad20dc4aafa58ed73eaccfec7b',
+    'news': '03faa47a95d84b63ab197e517f4ca71f',
+    'architect': '8309b2d169a449ad988d80445641d039',
+    'all-sources': '66a93e09f448459d8c6326ec9bcf1e12',
+    'sources-cn': 'a823011277854b04be6ee8d91682b55d',
+    'sources-En': '1f12bd7832be4748ae174a164348015d',
+    'sources-jp': '82d7df45126944598c4feec7dde0e34d',
+    'Filter-Bubble': '643a00d4a286463b804b3d19419e3e9e',
+    'reeder-4': 'b62c9c5bc30d4d7f88a5979d268c234e',
+    'english': 'RSS-Source-RSS-feeds-recommendation-902a1f504fa441c4b82ed1a84f3fe122'
+  }
 }
